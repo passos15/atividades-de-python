acee392711340e5903fef3a0c84a4da43b62636f
Erika <erikapassos2421@gmail.com>
2026-04-16 16:59:29 -0300

    atividade 1

---
+++ atvidade1.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome: ')
+print(f'olá {nome}! Bem-vindo(a) ao mundo Python !') 
+#'nome' é uma variável usada para guardar o valor digitado pelo usuário.
+#input, para pedir uma informação ao usuário.
+# O f ativa a "formatação", permitindo usar variáveis entre chaves {}.
