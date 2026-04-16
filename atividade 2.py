7513da475a9e9c5c920fac41ec7be3b93193cce3
Erika <erikapassos2421@gmail.com>
2026-04-16 17:33:29 -0300

    atividade 2

---
+++ atividade 2.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um numero :'))
+n2 = int(input('Digite outro numero :'))
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+print(f'A soma é {soma}, a subtração é {subtracao}, a multiplicação é {multiplicacao} e a divisão é {divisao}')
+#Para calcular é preciso usar o float ou o int antes do input, caso contrário o resultado será entendido como uma palavra.
+#As operações de soma, subtração, multiplicação e divisão são feitas usando os símbolos +, -, * e / respectivamente.
