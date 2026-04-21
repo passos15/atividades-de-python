6e553be0b48b879878a4dcf6116c3211893b2665
Erika <erikapassos2421@gmail.com>
2026-04-21 13:16:23 -0300

    atividade 3

---
+++ atividade 3.py
@@ -0,0 +1,8 @@
+n = float(input('Digite um numero :'))
+if n %2==0:
+#if é usado para criar uma condição, ou seja, para dizer que algo só deve acontecer se uma determinada situação for verdadeira.
+    print('O numero é par')
+else:  
+#else é usado para dizer o que deve acontecer se a condição do if for falsa.
+     print('O numero é impar')
+#O símbolo % calcula o resto da divisão, ou seja, se um número é divisível por 2, o resto será 0, indicando que é par. Se o resto for diferente de 0, o número é ímpar.
