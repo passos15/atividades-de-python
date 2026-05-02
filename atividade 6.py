2dafa2b595a0c5e8f87434092fc680ab3b5813d4
Erika <erikapassos2421@gmail.com>
2026-05-02 18:06:27 -0300

    atualização

--- atividade 6.py
+++ atividade 6.py
@@ -1,13 +1,14 @@
 lista_compras = []
 
 while True:
-    print("\n----- Lista de compras -----\n")
-    print("1. Adicionar item a lista")
-    print("2. Remover item da lista")
-    print("3. Vizualizar lista")
+    print("-----LISTA DE COMPRAS-----")
+    print("1. Adicionar itens a lista.")
+    print("2. Rempver itens da lista.")
+    print("3. Visualizar itens da lista.")
     print("4. Sair")
-    opcao = input("Digite a opção desejada: ")
     
+    opcao = input("Digite a opção desejada: ")
+
     if opcao == '1':
         item = input("Digite o item que deseja adicionar: ")
         lista_compras.append(item)
@@ -16,17 +17,17 @@ while True:
         item = input("Digite o item que deseja remover: ")
         if item in lista_compras:
             lista_compras.remove(item)
-            print(f"{item} removido da lista!")
+            print(f"{item} foi removido da lista de compras!")
         else:
-            print("Item não encontrado na lista.")
-    elif opcao  == '3':
+            print(f"{item} não encontrado na lista.")
+    elif opcao == '3':
         if len (lista_compras) == 0:
-            print(" A lista está vazia.")
+            print("A lista de compras está vazia!")
         else:
-            for indice, item in enumerate (lista_compras, start = 1):
-                print(f"{indice}.{item}")
+            for i, item in enumerate (lista_compras):
+               print(f"{i+1}.{item}")
     elif opcao == '4':
-        print("Saindo do programa!")
+        print("Saindo da lista...")
         break
     else:
-        print("opção não encontrada, tente novamente.")
+        print("Opção não encontrada, tente novamente.")
