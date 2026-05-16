2b7d51bc771d186e326aaf662dd1ac1dbd93669a
Erika <erikapassos2421@gmail.com>
2026-05-16 17:34:11 -0300

    atividade 9

---
+++ atividade 9.py
@@ -0,0 +1,15 @@
+def fatorial(n):
+#O def é usado para definir uma função;
+    if n == 0 or n == 1:
+        return 1
+#Se o número for igual a 0 ou 1, retornará para 1; assim, o código não rodará sem parar.
+    else:
+#Se o número for acima de 1, o fatorial será calculado.
+        return n * fatorial(n-1)
+numero = int(input("Digite um número inteiro positivo para calcular o fatorial: "))
+#O if  servirá para mostrar que números negativos não tem fatorial.
+if numero <0:
+    print("Fatorial não é definido para números negativos.")
+#O else mostrará o resultado da equação.
+else:
+    print(f"O fatorial de um {numero} é {fatorial(numero)}.")
